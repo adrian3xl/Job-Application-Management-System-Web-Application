48835bb57e2677b8d8c90f2dd10a263cf1e67dd4
adrian3xl <59806075+adrian3xl@users.noreply.github.com>
2020-03-16 21:43:44 -0500

    added first class

---
+++ JobApp(Web)/Data/Jobseeker.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobApp_Web_.Data
+{
+    public class Jobseeker: IdentityUser 
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+    }
+}
