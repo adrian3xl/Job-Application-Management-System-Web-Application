67dd1cd59464104f20117eff133fba14e5c4d878
adrian3xl <59806075+adrian3xl@users.noreply.github.com>
2020-03-16 22:17:26 -0500

    add more tables

--- JobApp(Web)/Data/Vacancy_Application.cs
+++ JobApp(Web)/Data/Vacancy_Application.cs
@@ -7,5 +7,16 @@ namespace JobApp_Web_.Data
 {
     public class Vacancy_Application
     {
+
+        public int Id { get; set; }
+        public string Job_title { get; set; }
+        public string Job_Discription { get; set; }
+        public string Job_Requirements { get; set; }
+        public int Job_level { get; set; }
+        public string Employment_type { get; set; }
+        public DateTime Submit_deadline { get; set; }
+        public string Job_category { get; set; }
+
+        public  Employer Employer { get; set; }
     }
 }
