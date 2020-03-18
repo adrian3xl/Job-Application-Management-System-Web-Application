e971a109b3b3bef85d2b03aabb66cf0e661f6205
adrian3xl <59806075+adrian3xl@users.noreply.github.com>
2020-03-18 08:51:00 -0500

    Migrated to database

--- JobApp(Web)/Data/Migrations/20200318134916_Application_details_table.Designer.cs
+++ JobApp(Web)/Data/Migrations/20200318134916_Application_details_table.Designer.cs
@@ -4,14 +4,16 @@ using JobApp_Web_.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace JobApp_Web_.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200318134916_Application_details_table")]
+    partial class Application_details_table
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
