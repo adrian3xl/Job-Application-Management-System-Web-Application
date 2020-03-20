b5fe7c64931b8e211cc271928c5631b65b1b75e5
adrian3xl <59806075+adrian3xl@users.noreply.github.com>
2020-03-19 23:00:57 -0500

    added repository

---
+++ JobApp(Web)/Contracts/IRepositoryBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobApp_Web_.Contracts
+{
+   public interface IRepositoryBase<T> where T : class
+    {
+        ICollection<T> FindAll();
+
+        T FindById(int id);
+
+        bool Create(T Entity);
+        bool Update(T Entity);
+        bool Delete(T Entity);
+
+        bool Save();
+    }
+}
