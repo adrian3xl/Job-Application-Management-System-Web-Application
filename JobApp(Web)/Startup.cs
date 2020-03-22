dc05f27320d24119418bd264eb102d6f5dd506b3
adrian3xl <59806075+adrian3xl@users.noreply.github.com>
2020-03-22 01:30:47 -0500

    identityuser

--- JobApp(Web)/Startup.cs
+++ JobApp(Web)/Startup.cs
@@ -42,7 +42,7 @@ namespace JobApp_Web_
 
             services.AddAutoMapper(typeof(Maps));
 
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<IdentityUser>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
