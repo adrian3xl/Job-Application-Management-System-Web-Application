c9e29663ef073067a803e5dfd9bff3efb0ef5d8c
adrian3xl <59806075+adrian3xl@users.noreply.github.com>
2020-03-29 18:17:56 -0500

    trying to generate a detail view

--- JobApp(Web)/Models/Vacancy_ApplicationVM.cs
+++ JobApp(Web)/Models/Vacancy_ApplicationVM.cs
@@ -21,10 +21,10 @@ namespace JobApp_Web_.Models
 
         public string Application_status { get; set; }
 
-        [Key]
-        public IEnumerable<SelectListItem> Vacancy_requests { get; set; }
-       [Key]
-        public IEnumerable<SelectListItem> Resume_requests { get; set; }
+    //    [Key]
+     //   public IEnumerable<SelectListItem> Vacancy_requests { get; set; }
+     //  [Key]
+    //    public IEnumerable<SelectListItem> Resume_requests { get; set; }
     }
 
     public class VacancyApplicationAdminViewVM
